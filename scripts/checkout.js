e04933b999ee19125367e4196bd4e7524e5c45e8
Ismat Aghamirzayev <ismataga1@gmail.com>
2024-12-22 17:03:13 +0400

    added cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem) => {
   
   
  cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${machingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -104,6 +105,11 @@ document.querySelector('.js-delete-link'
     .forEach((link)=>{
         link.addEventListener('click',()=>{
           const productId=link.dataset.productId;
+          removeFromCart(productId);
+
+         const container= document.querySelector(
+            `.js-cart-item-container-${productId}`
+          )
             
         })
     })
